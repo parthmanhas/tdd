227d0e9e0f085190f461ec6fcb55bc291cb5a198
parthmanhas <parthmanhas@gmail.com>
2021-05-28 22:48:26 +0530

    progress

---
+++ src/AppointmentForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const AppointmentForm = ({ selectableServices }) => (
+    <form id="appointment">
+        <select name="service">
+            <option />
+            {selectableServices.map(s => (
+                <option key={s}>{s}</option>
+            ))}
+        </select>
+    </form>
+);
